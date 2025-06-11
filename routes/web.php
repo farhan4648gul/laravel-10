bb487d6b3c7caffa490b104cea1a3f7345b89acc
Farhan Ubuntu 24.04.2 LTS <farhan4648gul@gmail.com>
2025-06-11 18:09:14 +0500

    using Breeze to add scaffoldings... :P

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [TestController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/contact', [ContactController::class, 'index']); 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-Route::get ('/contact/store', [TestController::class, 'store']); 
-
-Route::get('/contact/createSeeker', [TestController::class, 'createSeeker'])->name('create.seeker');  
-
-
+require __DIR__.'/auth.php';
